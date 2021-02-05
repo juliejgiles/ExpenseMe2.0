b819c82488564eada87f9afb9663ef609159d161
Luke <luke.milsom1@hotmail.com>
2021-02-05 13:33:12 +1100

    Dashboard feature fully functional, with carousel and different graph implementation

--- client/src/components/IncomeExpenses.js
+++ client/src/components/IncomeExpenses.js
@@ -21,14 +21,16 @@ export const IncomeExpenses = () => {
   ).toFixed(2);
 
   return (
-    <div className="inc-exp-container">
+    <div className='inc-exp-container'>
       <div>
         <h4>Income</h4>
-        <p className="money plus">{numberWithCommas(income)}</p>
+        <p className='money plus'>{numberWithCommas(income)}</p>
       </div>
-      <div>
-        <h4>Expense</h4>
-        <p className="money minus">{numberWithCommas(expense)}</p>
+      <div className='inc-exp-containerOne'>
+        <div>
+          <h4>Expense</h4>
+          <p className='money minus'>{numberWithCommas(expense)}</p>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/MakeEntry.js
+++ client/src/pages/MakeEntry.js
@@ -1,11 +1,20 @@
-import React from "react";
 import { Link } from "react-router-dom";
-
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Balance } from "../components/Balance";
+import { IncomeExpenses } from "../components/IncomeExpenses";
+import { TransactionList } from "../components/TransactionList";
+import { GlobalProvider } from "../context/GlobalState";
+import { AddTransaction } from "../components/AddTransaction";
 export const MakeEntry = () => {
   return (
     <div>
-      <h2>Make an entry</h2>
-      <Link to="/">Back</Link>
+      <GlobalProvider>
+        <h2>Make an entry</h2>
+        <AddTransaction />
+
+        <Link to='/'>Back</Link>
+      </GlobalProvider>
     </div>
   );
 };
